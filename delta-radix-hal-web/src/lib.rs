d5bc14fed6411f20e331c46bfde829f0e1f43aad
Aaron Christiansen <aaronc20000@gmail.com>
2023-02-15 19:52:46 +0000

    Ah, that's it - you can't have a function named `main`

--- delta-radix-hal-web/src/lib.rs
+++ delta-radix-hal-web/src/lib.rs
@@ -3,7 +3,7 @@ use wasm_bindgen::prelude::wasm_bindgen;
 mod hal;
 
 #[wasm_bindgen]
-pub async fn main() {
+pub async fn start_os() {
     let hal = hal::WebHal::new();
     delta_radix_os::main(hal).await
 }
