c1c17132868c514707aac2ab117d5820411adaa7
Aaron Christiansen <aaronc20000@gmail.com>
2023-02-17 17:27:34 +0000

    Implement evaluation for all nodes (though no parsing yet)

--- delta-radix-os/src/calc/eval.rs
+++ delta-radix-os/src/calc/eval.rs
@@ -36,16 +36,22 @@ pub fn evaluate(node: &Node, config: &Configuration) -> EvaluationResult {
     match &node.kind {
         NodeKind::Number(num) => EvaluationResult::new(num.clone(), false),
         
-        NodeKind::Add(a, b) => {
+        NodeKind::Add(a, b)
+        | NodeKind::Subtract(a, b)
+        | NodeKind::Divide(a, b)
+        | NodeKind::Multiply(a, b) => {
             let a = evaluate(a, config);
             let b = evaluate(b, config);
 
-            let (result, overflow) = a.result.add(&b.result, config.data_type.signed);
+            let (result, overflow) = match &node.kind {
+                NodeKind::Add(_, _) => a.result.add(&b.result, config.data_type.signed),
+                NodeKind::Subtract(_, _) => a.result.subtract(&b.result, config.data_type.signed),
+                NodeKind::Multiply(_, _) => a.result.multiply(&b.result, config.data_type.signed),
+                NodeKind::Divide(_, _) => a.result.divide(&b.result, config.data_type.signed),
+                _ => unreachable!()
+            };
+
             EvaluationResult::new(result, a.overflow || b.overflow || overflow)
         },
-        
-        NodeKind::Subtract(a, b) => todo!(),
-        NodeKind::Divide(a, b) => todo!(),
-        NodeKind::Multiply(a, b) => todo!(),
     }
 }
