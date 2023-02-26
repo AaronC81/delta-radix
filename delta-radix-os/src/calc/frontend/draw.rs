8c7c28401837a1ff5d72e324d076130c321e3b61
Aaron Christiansen <aaronc20000@gmail.com>
2023-02-26 12:24:58 +0000

    Break up frontend files

---
+++ delta-radix-os/src/calc/frontend/draw.rs
@@ -0,0 +1,177 @@
+use alloc::vec::Vec;
+use delta_radix_hal::{Hal, Display, DisplaySpecialCharacter};
+
+use crate::calc::backend::parse::ConstantOverflowChecker;
+
+use super::{CalculatorApplication, ApplicationState};
+
+
+impl<'h, H: Hal> CalculatorApplication<'h, H> {
+    pub fn draw_full(&mut self) {
+        self.hal.display_mut().clear();
+        match self.state {
+            ApplicationState::Normal | ApplicationState::OutputBaseSelect | ApplicationState::VariableSet => {
+                self.draw_header();
+                self.draw_expression();
+                self.draw_result();
+            }
+
+            ApplicationState::FormatMenu { ref bits_digits, bits_cursor_pos } => {
+                let display = self.hal.display_mut();
+                let bits_header = "Bits: ";
+
+                display.set_position((bits_header.len() as u8 + bits_cursor_pos as u8) - 1, 0);
+                display.print_special(DisplaySpecialCharacter::CursorLeft);
+                display.print_special(DisplaySpecialCharacter::CursorRight);
+
+                display.set_position(0, 1);
+                display.print_string(bits_header);
+                display.print_string(bits_digits);
+
+                display.set_position(0, 2);
+                display.print_string("-) Signed  ");
+                if self.eval_config.data_type.signed {
+                    display.print_string(" <");
+                }
+                display.set_position(0, 3);
+                display.print_string("+) Unsigned");
+                if !self.eval_config.data_type.signed {
+                    display.print_string(" <");
+                }
+            }
+
+            ApplicationState::OutputSignedMenu => {
+                let display = self.hal.display_mut();
+
+                display.clear();
+                display.print_string("Ans signedness ovrd.");
+
+                display.set_position(0, 1);
+                display.print_string("DEL) None    ");
+                if self.signed_result.is_none() { display.print_string(" <"); }
+
+                display.set_position(0, 2);
+                display.print_string("  -) Signed  ");
+                if self.signed_result == Some(true) { display.print_string(" <"); }
+
+                display.set_position(0, 3);
+                display.print_string("  +) Unsigned");
+                if self.signed_result == Some(false) { display.print_string(" <"); }
+            }
+        }
+    }
+    
+    pub fn draw_header(&mut self) {
+        let disp = self.hal.display_mut();
+        disp.set_position(0, 0);
+
+        let name = self.eval_config.data_type.concise_name();
+        disp.print_string(&name);
+        let mut format_len = name.len();
+
+        if let Some(sign) = self.signed_result {
+            disp.print_char('>');
+            disp.print_char(if sign { 'S' } else { 'U' });
+            format_len += 2;
+        }
+
+        disp.print_char(' ');
+
+        let has_overflow = if let Some(Ok(r)) = &self.eval_result {
+            r.overflow || self.constant_overflows
+        } else {
+            false
+        };
+        let overflow_marker = " OVER";
+
+        let mut ptr = format_len + 1;
+        let ptr_target = if has_overflow { Self::WIDTH - overflow_marker.len() } else { Self::WIDTH };
+        while ptr < ptr_target {
+            if self.input_shifted {
+                disp.print_char('^');
+            } else {
+                disp.print_char('=');
+            }
+            ptr += 1;
+        }
+
+        if has_overflow {
+            disp.print_string(overflow_marker);
+        }
+    }
+
+    pub fn draw_expression(&mut self) {
+        self.adjust_scroll();
+
+        // Try to parse and get warning spans
+        let (parser, _) = self.parse::<ConstantOverflowChecker>();
+        let warning_indices = parser.constant_overflow_spans.iter()
+            .flat_map(|s| s.indices().collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+
+        self.constant_overflows = !warning_indices.is_empty();
+        
+        let disp = self.hal.display_mut();
+
+        // Draw expression
+        disp.set_position(0, 2);
+        let mut chars_written = 0;
+        for glyph in self.glyphs.iter().skip(self.scroll_offset).take(Self::WIDTH) {
+            disp.print_glyph(*glyph);
+            chars_written += 1;
+        }
+        for _ in chars_written..Self::WIDTH {
+            disp.print_char(' ');
+        }
+
+        // Draw cursor
+        disp.set_position(0, 1);
+        for i in self.scroll_offset..(self.scroll_offset + Self::WIDTH) {
+            let warn = warning_indices.contains(&i);
+            if i + 1 == self.cursor_pos {
+                if warn {
+                    disp.print_special(DisplaySpecialCharacter::CursorLeftWithWarning)
+                } else {
+                    disp.print_special(DisplaySpecialCharacter::CursorLeft)
+                }
+            } else if i == self.cursor_pos {
+                if warn {
+                    disp.print_special(DisplaySpecialCharacter::CursorRightWithWarning)
+                } else {
+                    disp.print_special(DisplaySpecialCharacter::CursorRight)
+                }
+            } else {
+                if warn {
+                    disp.print_special(DisplaySpecialCharacter::Warning)
+                } else {
+                    disp.print_char(' ')
+                }
+            }
+        }
+    }
+
+    pub fn draw_result(&mut self) {
+        let disp = self.hal.display_mut();
+
+        if self.state == ApplicationState::OutputBaseSelect {
+            disp.set_position(0, 3);
+            disp.print_string("BASE? ");
+            return;
+        }
+
+        if self.state == ApplicationState::VariableSet {
+            disp.set_position(0, 3);
+            disp.print_string("SET? ");
+            return;
+        }
+
+        // Briefly drop and re-borrow the display so we can call a method on `&self`
+        drop(disp);
+        let str = self.eval_result_to_string()
+            .unwrap_or_else(|| str::repeat(" ", Self::WIDTH));
+        let disp = self.hal.display_mut();
+
+        disp.set_position((Self::WIDTH - str.len()) as u8, 3);
+        disp.print_string(&str);
+    }
+}
---
+++ delta-radix-os/src/calc/frontend/input.rs
@@ -0,0 +1,230 @@
+use alloc::string::ToString;
+use delta_radix_hal::{Hal, Key, Glyph};
+
+use crate::menu;
+
+use super::{CalculatorApplication, ApplicationState, Base};
+
+impl<'h, H: Hal> CalculatorApplication<'h, H> {
+    pub async fn process_input_and_redraw(&mut self, key: Key) {
+        if menu::check_menu(self.hal, key, self.input_shifted).await {
+            self.draw_full();
+            return
+        }
+
+        match self.state {
+            ApplicationState::Normal =>
+                if self.input_shifted {
+                    match key {
+                        Key::Shift => {
+                            self.input_shifted = false;
+                            self.draw_header();
+                        }
+                        Key::Delete => {
+                            self.clear_all(true);
+                            self.draw_full();
+                        }
+
+                        Key::Digit(0) => {
+                            self.input_shifted = false;
+                            
+                            // Insert two characters and move between them
+                            self.glyphs.insert(self.cursor_pos, Glyph::LeftParen);
+                            self.cursor_pos += 1;
+                            self.glyphs.insert(self.cursor_pos, Glyph::RightParen);
+                            self.draw_expression();
+                            self.clear_evaluation(true);
+                        }
+
+                        Key::Variable => {
+                            self.input_shifted = false;
+                            if let Some(Ok(_)) = self.eval_result {
+                                self.state = ApplicationState::VariableSet;
+                                self.draw_header();
+                                self.draw_result();
+                            } else {
+                                self.draw_full();
+                            }
+                        }
+
+                        Key::FormatSelect => {
+                            self.input_shifted = false;
+                            self.state = ApplicationState::OutputSignedMenu;
+                            self.draw_full();
+                        }
+
+                        _ => (),
+                    }
+                } else {
+                    match key {
+                        Key::Digit(d) => self.insert_and_redraw(Glyph::Digit(d)),
+                        Key::HexBase => self.insert_and_redraw(Glyph::HexBase),
+                        Key::BinaryBase => self.insert_and_redraw(Glyph::BinaryBase),
+            
+                        Key::Add => self.insert_and_redraw(Glyph::Add),
+                        Key::Subtract => self.insert_and_redraw(Glyph::Subtract),
+                        Key::Multiply => self.insert_and_redraw(Glyph::Multiply),
+                        Key::Divide => self.insert_and_redraw(Glyph::Divide),
+
+                        // TODO: nicer insertion mechanism, and treat as one token?
+                        Key::Variable => self.insert_and_redraw(Glyph::Variable),
+            
+                        Key::Left => {
+                            if self.cursor_pos > 0 {
+                                self.cursor_pos -= 1;
+                                self.draw_expression();
+                            }
+                        },
+                        Key::Right => {
+                            if self.cursor_pos < self.glyphs.len() {
+                                self.cursor_pos += 1;
+                                self.draw_expression();
+                            }
+                        }
+                        Key::Delete => {
+                            if self.cursor_pos > 0 {
+                                self.cursor_pos -= 1;
+                                self.glyphs.remove(self.cursor_pos);
+                                self.draw_expression();
+                                self.clear_evaluation(true);
+                            }
+                        },
+                        Key::Exe => {
+                            self.evaluate();
+                            self.draw_result();
+                            self.draw_header();
+                        }
+
+                        Key::FormatSelect => {
+                            self.state = ApplicationState::OutputBaseSelect;
+                            self.draw_result();
+                        }
+            
+                        Key::Shift => {
+                            self.input_shifted = true;
+                            self.draw_header();
+                        }
+
+                        Key::Sleep => {
+                            // Do not redraw - the HAL is expected to deal with this
+                            self.clear_all(false);
+                        }
+
+                        Key::Menu => {
+                            let bits_digits = self.eval_config.data_type.bits.to_string();
+                            self.state = ApplicationState::FormatMenu {
+                                bits_cursor_pos: bits_digits.len(),
+                                bits_digits,
+                            };
+                            self.draw_full();
+                        }
+                        
+                        Key::DebugTerminate => (),
+                    }
+                },
+            
+            ApplicationState::OutputBaseSelect => match key {
+                Key::HexBase => self.set_output_format_and_redraw(Base::Hexadecimal),
+                Key::BinaryBase => self.set_output_format_and_redraw(Base::Binary),
+                Key::FormatSelect => self.set_output_format_and_redraw(Base::Decimal),
+
+                _ => (),
+            }
+
+            ApplicationState::VariableSet => match key {
+                Key::Digit(d) => {
+                    self.variables[d as usize] = Glyph::from_string(&self.eval_result_to_string().unwrap()).unwrap();
+
+                    self.state = ApplicationState::Normal;
+                    self.draw_full();
+                },
+
+                Key::Exe | Key::Variable => {
+                    self.state = ApplicationState::Normal;
+                    self.draw_full();
+                }
+
+                _ => (),
+            }
+
+            ApplicationState::FormatMenu { ref mut bits_digits, ref mut bits_cursor_pos } => match key {
+                Key::Digit(d) => {
+                    bits_digits.push(char::from_digit(d as u32, 10).unwrap());
+                    *bits_cursor_pos += 1;
+                    self.draw_full();
+                }
+
+                Key::Delete => {
+                    if *bits_cursor_pos > 0 {
+                        bits_digits.remove(*bits_cursor_pos - 1);
+                        *bits_cursor_pos -= 1;
+                        self.draw_full();
+                    }
+                }
+                Key::Left => {
+                    if *bits_cursor_pos > 0 {
+                        *bits_cursor_pos -= 1;
+                        self.draw_full();
+                    }
+                }
+                Key::Right => {
+                    if *bits_cursor_pos < bits_digits.len() {
+                        *bits_cursor_pos += 1;
+                        self.draw_full();
+                    }
+                }
+
+                Key::Add => {
+                    self.eval_config.data_type.signed = false;
+                    self.draw_full();
+                }
+                Key::Subtract => {
+                    self.eval_config.data_type.signed = true;
+                    self.draw_full();
+                }
+
+                Key::FormatSelect | Key::Menu | Key::Exe => {
+                    // Apply bits evaluation settings
+                    if let Ok(mut bits) = bits_digits.parse() {
+                        // Minimum supported number of bits
+                        if bits < 3 {
+                            bits = 3;
+                        }
+
+                        self.eval_config.data_type.bits = bits;
+                    }
+
+                    self.state = ApplicationState::Normal;
+                    self.clear_evaluation(true);
+                    self.draw_full();
+                }
+
+                _ => (),
+            }
+
+            ApplicationState::OutputSignedMenu => match key {
+                Key::Delete => {
+                    self.signed_result = None;
+                    self.draw_full();
+                }
+                Key::Add => {
+                    self.signed_result = Some(false);
+                    self.draw_full();
+                }
+                Key::Subtract => {
+                    self.signed_result = Some(true);
+                    self.draw_full();
+                }
+
+                Key::FormatSelect | Key::Menu | Key::Exe => {
+                    self.state = ApplicationState::Normal;
+                    self.clear_evaluation(true);
+                    self.draw_full();
+                }
+
+                _ => (),
+            }
+        }
+        
+    }
+}
